b4fc5a15d11cfca0ee62419d1a8cd3e47e18d3ba
jeffpamer <jeff@mediumdivision.com>
2015-06-27 13:32:32 -0400

    Basic search and list results for org repositories

--- app/main.js
+++ app/main.js
@@ -1,10 +1,37 @@
 var React = window.React = require('react');
+var xhr = require('xr');
+var R = require('ramda');
 
 var App = React.createClass({
 
+    getInitialState: function() {
+        return { repos: [] };
+    },
+
+    onSubmit: function() {
+        var orgName = React.findDOMNode(this.refs.organizationName).value;
+
+        xhr.get(`https://api.github.com/orgs/${orgName}/repos`)
+        .then((response) => {
+            this.setState({ repos: response.data });
+        });
+    },
+
+    renderRepositories: function() {
+        return <ul>
+            {R.map((repo) => {
+                return <li key={repo.id}>{repo.name}</li>;
+            }, this.state.repos)}
+        </ul>;
+    },
+
     render: function() {
         return (
-            <h1>Hello World.</h1>
+            <div>
+                <input type="text" ref="organizationName" />
+                <button ref="searchButton" onClick={this.onSubmit}>Search</button>
+                {this.renderRepositories()}
+            </div>
         );
     }
 
