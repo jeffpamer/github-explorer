66f545259ea1d337b07b6c55a4ad0ff9605b67a1
jeffpamer <jeff@mediumdivision.com>
2015-06-27 12:44:07 -0400

    Basic webpack bundling setup. App running locally with webpack-dev-server

--- server.js
+++ server.js
@@ -1,16 +1,35 @@
 var express = require('express');
 var path = require('path');
+var httpProxy = require('http-proxy');
 
+var proxy = httpProxy.createProxyServer();
 var app = express();
 
 var isProduction = process.env.NODE_ENV === 'production';
-var port = isProduction ? 8080 : 3000;
+var port = isProduction ? 8080 : 3001;
 var publicPath = path.resolve(__dirname, 'public');
 
-// Static assets
 app.use(express.static(publicPath));
 
-// Server
+if (!isProduction) {
+
+  var bundle = require('./server/bundle.js');
+  bundle();
+
+  // Any requests to localhost:3000/build is proxied
+  // to webpack-dev-server
+  app.all('/build/*', function (req, res) {
+    proxy.web(req, res, {
+        target: 'http://localhost:8080'
+    });
+  });
+
+}
+
+proxy.on('error', function(e) {
+  console.log('Could not connect to proxy, please try again...');
+});
+
 app.listen(port, function () {
-    console.log('Server running on port ' + port);
+  console.log('Server running on port ' + port);
 });
